00fe8e45d689bc69ca72d3a2197c30e66c8a0e85
Ye Junzhe <ye.junzhe@outlook.com>
2023-11-26 12:59:08 +0800

    (1) Add README.md (2) Modified run.sh

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bindgen C to Rust
+
+Example project to demonstrate how calling C code in Rust works
--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+cargo install bindgen
+
 bindgen c_struct.h > rust_bindings/bindgen_from_c.rs
 
 rustc rust_main.rs
+
+./rust_main
